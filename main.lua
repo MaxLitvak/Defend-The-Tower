ad1d42e314fb6c40192b893f29b7a8271785a329
Max Litvak <max_litvak20@milton.edu>
2019-05-31 13:55:08 -0400

    hi

--- main.lua
+++ main.lua
@@ -1,32 +1,33 @@
 function love.load()
-  width = 800
+  width = 1000
   height = 600
-  love.window.setMode(width, height, {resizable=true, vsync=false, minwidth=400, minheight=300})
+  love.window.setMode(width, height, {resizable=true, vsync=true, minwidth=400, minheight=300})
   tower = love.graphics.newImage("tower.png")
   luegi = love.graphics.newImage("luegi.jpeg")
   xpos = 200
   ypos = 200
+  speed = 10
 end
 
 function love.update(dt)
 	if xpos > 0.3 then
 		if love.keyboard.isDown('left') then
-			xpos = xpos - 0.3
+			xpos = xpos - speed
 		end
 	end
 	if xpos < width - 0.3 then
 		if love.keyboard.isDown('right') then
-			xpos = xpos + 0.3
+			xpos = xpos + speed
 		end
 	end
 	if ypos > 0.3 then
 		if love.keyboard.isDown('up') then
-			ypos = ypos - 0.3
+			ypos = ypos - speed
 		end
 	end
 	if ypos < height - 100 then
 		if love.keyboard.isDown('down') then
-			ypos = ypos + 0.3
+			ypos = ypos + speed
 		end
 	end
 end
